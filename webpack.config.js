6abd1e419fdf3e8d7fc1990361a7124bf1a2b733
Sahat Yalkabov <sakhat@gmail.com>
2016-07-22 22:15:58 -0700

    removed hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-hot-middleware/client',
     './app/main'
   ],
   output: {
@@ -13,7 +12,6 @@ module.exports = {
     publicPath: '/dist/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
   module: {
